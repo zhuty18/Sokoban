65564de51eaaf0d2e14d48cf18012e1710875c99
zhuty18 <zhuty18@mails.tsinghua.edu.cn>
2020-09-30 20:51:05 +0800

    设计了新手导引部分关卡，失去灵感

--- src/mySokoban/Main.java
+++ src/mySokoban/Main.java
@@ -2,7 +2,7 @@ package mySokoban;
 
 public class Main {
 	public static void main(String... argv) {
-		MainFrame win=new MainFrame("数独游戏");
+		MainFrame win=new MainFrame("推箱子游戏");
 		win.getHelp();
 	}
 }
